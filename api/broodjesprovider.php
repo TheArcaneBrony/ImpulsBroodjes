f15c3d444e96e5071705aaff917016831343af54
The Arcane Brony <myrainbowdash949@gmail.com>
2021-02-24 11:44:23 +0100

    Read items from database

--- api/broodjesprovider.php
+++ api/broodjesprovider.php
@@ -7,7 +7,7 @@ $mysqli = new mysqli(DOMAIN, USERNAME, PASSWORD, SCHEMA);
 function getBroodjes() {
     // mysqli_query()
     global $mysqli;
-    $stmt = mysqli_prepare($mysqli, "select * from impulsbroodjes.broodjes where broodje_type = '';");
+    $stmt = mysqli_prepare($mysqli, "select * from impulsbroodjes.broodjes where broodje_type = 'broodje';");
     //$stmt = $mysqli->prepare("INSERT INTO CountryLanguage VALUES (?, ?, ?, ?)");
     //$stmt->bind_param('sssd', $code, $language, $official, $percent);
     if(!mysqli_stmt_execute($stmt)){
@@ -21,25 +21,23 @@ function getBroodjes() {
         $items[$i] = Broodje::new($item["broodje_name"], explode(", ", $item["broodje_ingredients"]), $item["broodje_price"], $item["broodje_image"]);
     }
     return $items;
-    //  return array(
-    //     Broodje::new("Broodje kaas",explode(", ", "kaas, mayo, ei, tomaat, komkommer, sla, wortel"),"3.40","https://www.managejekanker.nl/wp-content/uploads/2016/04/broodje-kaas-1.jpg"),
-    //     Broodje::new("Broodje mozarella extra",explode(", ", "mozzarella, pesto, olijven, zongedroogde tomaten, rucola"),"3.70","https://www.managejekanker.nl/wp-content/uploads/2016/04/broodje-kaas-1.jpg"),
-    //     Broodje::new("Broodje brie noten",explode(", ", "Brie, honing, noten, sla"),"3.70","https://f.jwwb.nl/public/m/y/q/temp-wnpqzwvhamcdljawxeam/0xhh3j/broodjebriehoningnoten.png"),
-    //     Broodje::new("Broodje geitenkaas appel",explode(", ", "geitenkaas, honing, pijnboompitten, appel, sla"),"3.90","https://i0.wp.com/etenmetroos.nl/wp-content/uploads/2017/08/geit.jpg?resize=900%2C450"),
-    //     Broodje::new("Broodje ham",explode(", ", "ham, ei, tomaat, komkommer, sla, wortel, mayo"),"3.50","https://www.hoogerbrugcuisine.nl/foto/47/1000/files/Afbeeldingen/zacht_wit_broodje_ham.jpg"),
-    //     Broodje::new("Broodje prepare",explode(", ", "Preparé, ei, tomaat, komkommer, wortel, sla, mayo"),"3.50","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzlRVEki80bEoRsSmxrJ906aBQqudVtnKHsg&usqp=CAU"),
-    //     Broodje::new("Broodje kippenwit",explode(", ", "kippenwit, ei, tomaat, komkommer, sla, wortel, mayo"),"3.60","https://broodjes-jipsy.be/wp-content/uploads/2014/08/Broodjes_Jipsy.jpg"),
-    //     Broodje::new("Broodje club",explode(", ", "préparé, augurk, tomaat, verse ui, sla, tartaarsaus"),"3.70","https://bakkerijaernoudt.be/sites/default/files/styles/large/public/club_00004.jpg?itok=xctOugAl"),
-    //     Broodje::new("Broodje smoske",explode(", ", "ham, kaas, ei, tomaat, komkommer, sla, wortel, mayo"),"3.60","https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Dagobert_au_jmbon_et_crudit%C3%A9s.jpg/266px-Dagobert_au_jmbon_et_crudit%C3%A9s.jpg"),
-    //     Broodje::new("Broodje martino",explode(", ", "préparé, augurk, ansjovis, zilveruitjes, tomaat, tabasco, sla, martinosaus"),"3.70","https://i2.wp.com/www.seasonwithlove.nl/wp-content/uploads/2014/11/broodjemartino.jpg?fit=1000%2C669&ssl=1")
-    // );
 }
 function getSalades(){
-    return array(
-        Broodje::new("Griekse salade ",explode(", ", "basis + feta kaas, ui, olijven, paprika, rucola"),"7.00","https://www.leukerecepten.nl/wp-content/uploads/2019/07/griekse-salade_v.jpg"),
-        Broodje::new("Kipsalade ",explode(", ", "basis + pasta, kippenblokjes, rode ui, olijven, kersttomaten"),"7.60","https://i0.wp.com/kitchenista.nl/wp-content/uploads/2017/01/kipsalade.jpg?resize=600%2C320&ssl=1"),
-        Broodje::new("Salade Nicoise ",explode(", ", "basis+ tonijn natuur, ui, paprika, ansjovis, bieslookdressing"),"8.00","https://img-3.journaldesfemmes.fr/8COrhFruV2keguuWQqaL0SoViws=/748x499/smart/3a86b25b4fd94561959d9ff592bce391/recipe-jdf/10025061.jpg")
-    );
+    global $mysqli;
+    $stmt = mysqli_prepare($mysqli, "select * from impulsbroodjes.broodjes where broodje_type = 'salade';");
+    //$stmt = $mysqli->prepare("INSERT INTO CountryLanguage VALUES (?, ?, ?, ?)");
+    //$stmt->bind_param('sssd', $code, $language, $official, $percent);
+    if(!mysqli_stmt_execute($stmt)){
+        echo "Error:<br>" . $mysqli->error;
+    }
+    $result = mysqli_stmt_get_result($stmt);
+    $rows = $result->num_rows;
+    $items = array();
+    for ($i=0; $i < $rows; $i++) { 
+        $item = mysqli_fetch_assoc($result);
+        $items[$i] = Broodje::new($item["broodje_name"], explode(", ", $item["broodje_ingredients"]), $item["broodje_price"], $item["broodje_image"]);
+    }
+    return $items;
 }
 class Broodje {
     public static function new($name, $ingredients, $price, $image){
